8f620a9480eaeff83aec03eed4e82b123b6f58a8
franck bambou <bamboufrancklio@gmail.com>
2024-05-07 12:16:02 +0200

    add gpt prompting

--- data/dataset_generators/gpt_prompts.py
+++ data/dataset_generators/gpt_prompts.py
@@ -1,14 +1,16 @@
 import torch
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from .base import DatasetGenerator
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
+# Initialisation du modèle et du tokenizer
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2LMHeadModel.from_pretrained('gpt2')
 
-
+# Configuration de padding
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = 'left'
 
+# Configuration de l'appareil
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 
@@ -16,7 +18,7 @@ class GptPromptsDatasetGenerator(DatasetGenerator):
     def __init__(
         self,
         generator,
-        batch_size=1,
+         batch_size=1,
         output_dir="dataset/train",
         num_images_per_label=200,
     ):
@@ -28,35 +30,25 @@ class GptPromptsDatasetGenerator(DatasetGenerator):
         adding = ["box", "cartons", "plates", "many"]
 
         for label in labels_names:
-            descriptions[label] = []
-            for add in adding:
-                prompt = f" description of an image of a {add} of {label} cheese "
+            prompts = [f"description of an image of a {add} of {label} cheese" for add in adding]
+            inputs = tokenizer(prompts, return_tensors='pt', padding=True, truncation=True, max_length=50)
+            input_ids = inputs['input_ids'].to(device)
+            attention_mask = inputs['attention_mask'].to(device)
 
-               
-                inputs = tokenizer.encode_plus(
-                    prompt, return_tensors='pt', padding='max_length', max_length=100, truncation=True
-                )
-                input_ids = inputs['input_ids'].to(device)
-                attention_mask = inputs['attention_mask'].to(device)
-
-                
+            with torch.no_grad():  
                 outputs = model.generate(
                     input_ids=input_ids,
                     attention_mask=attention_mask,
                     pad_token_id=tokenizer.pad_token_id,
-                     max_new_tokens=30
+                    max_new_tokens=30
                 )
-                description = tokenizer.decode(outputs[0], skip_special_tokens=True)
-                descriptions[label].append(description)
+            
+            generated_texts = [tokenizer.decode(output, skip_special_tokens=True) for output in outputs]
+            descriptions[label] = generated_texts
 
-        prompts = {}
-        for label in labels_names:
-            prompts[label] = []
-            elements = descriptions[label]
-            for e in elements:
-                prompts[label].append({
-                    "prompt": e,
-                    "num_images": self.num_images_per_label,
-                })
+        prompts = {
+            label: [{"prompt": description, "num_images": self.num_images_per_label} for description in descriptions[label]]
+            for label in labels_names
+        }
 
         return prompts
