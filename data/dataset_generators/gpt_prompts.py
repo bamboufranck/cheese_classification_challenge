d5adb005028658751fec11db002e4048cb7a1575
franck bambou <bamboufrancklio@gmail.com>
2024-05-07 11:40:53 +0200

    az

--- data/dataset_generators/gpt_prompts.py
+++ data/dataset_generators/gpt_prompts.py
@@ -1,10 +1,10 @@
-from .base import DatasetGenerator
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
-
-
+from .base import DatasetGenerator
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+# Définir pad_token pour l'encodeur
 tokenizer.pad_token = tokenizer.eos_token
 
 class GptPromptsDatasetGenerator(DatasetGenerator):
@@ -19,36 +19,39 @@ class GptPromptsDatasetGenerator(DatasetGenerator):
         self.num_images_per_label = num_images_per_label
 
     def create_prompts(self, labels_names):
-
         descriptions = {}
-        adding=["box","cartons","plates","many"]  #ajout
+        adding = ["box", "cartons", "plates", "many"]
 
         for label in labels_names:
             descriptions[label] = []
             for add in adding:
-                prompt=f"An image of a {add} of {label} cheese"
+                prompt = f"An image of a {add} of {label} cheese"
 
-                
-                inputs = tokenizer.encode_plus(prompt, return_tensors='pt', padding='max_length', max_length=50, truncation=True)
+                # Encoder les inputs correctement
+                inputs = tokenizer.encode_plus(
+                    prompt, return_tensors='pt', padding='max_length', max_length=50, truncation=True
+                )
                 input_ids = inputs['input_ids']
                 attention_mask = inputs['attention_mask']
-                input_ids = tokenizer.encode(prompt, return_tensors='pt')
-                outputs = model.generate(input_ids=input_ids,attention_mask=attention_mask,pad_token_id=tokenizer.pad_token_id,max_length=50
-)
+
+                # Générer les sorties
+                outputs = model.generate(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask,
+                    pad_token_id=tokenizer.pad_token_id,
+                    max_length=50
+                )
                 description = tokenizer.decode(outputs[0], skip_special_tokens=True)
                 descriptions[label].append(description)
-        
+
         prompts = {}
-        elements=[]
         for label in labels_names:
             prompts[label] = []
-            elements=descriptions[label]
+            elements = descriptions[label]
             for e in elements:
-                prompts[label].append(
-                {
-                    "prompt": e,  #ajout
+                prompts[label].append({
+                    "prompt": e,
                     "num_images": self.num_images_per_label,
-                }
-            )
-                
+                })
+
         return prompts
