0e8ac792fa3768c92d01e71c8e88d518c5efab0d
franck bambou <bamboufrancklio@gmail.com>
2024-05-08 01:02:50 +0200

    test

--- data/dataset_generators/clip_prompts.py
+++ data/dataset_generators/clip_prompts.py
@@ -4,7 +4,7 @@ from PIL import Image
 from .base import DatasetGenerator
 from tqdm import tqdm
 
-"""""
+
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
 
 
@@ -23,18 +23,8 @@ max_length = 50
 num_beams = 4
 gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
 
-"""""
-
-from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-
-model.to(device)
 
 class ClipPromptsDatasetGenerator(DatasetGenerator):
     def __init__(
@@ -71,26 +61,9 @@ class ClipPromptsDatasetGenerator(DatasetGenerator):
             pixel_values = pixel_values.to(device)
 
            
-           # start prompt caption and new elements
-            prompt = f"describe this image of a {maping[valeur_label]} cheese :"
-
-            input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-            
-            input_ids = input_ids.to(device)
-
-
-            generated_ids = model.generate(pixel_values=pixel_values,decoder_input_ids=input_ids,max_new_tokens=16)
-
-            descript = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
-
-            description= descript + " " + prompt
-
-           
-            # end of new
-
-            #output_ids = model.generate(pixel_values, **gen_kwargs)
-            #descriptions = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-            #description = descriptions[0].strip()+ " " + f"a piece or a box of {maping[valeur_label]}"
+            output_ids = model.generate(pixel_values, **gen_kwargs)
+            descriptions = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+            description = descriptions[0].strip()+ " " + f" describe a cheese {maping[valeur_label]}"
 
            
 
