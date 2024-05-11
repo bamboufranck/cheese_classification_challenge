d896972d442d867686b6aee90e6c49c19f31373e
franck bambou <bamboufrancklio@gmail.com>
2024-05-11 07:08:31 +0200

    fosso

--- data/dataset_generators/base.py
+++ data/dataset_generators/base.py
@@ -25,13 +25,14 @@ class DatasetGenerator:
 
     def generate(self, labels_names,val_data,maping):
 
-        
+        """""
         model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")   # Ajout
         processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")   # Ajout
 
         labels_names_with_cheese = [name + " cheese" for name in labels_names]
         text_inputs = processor(text=labels_names_with_cheese, return_tensors="pt", padding=True) # Ajout
 
+        """""
         
 
         # ensuite ici fine tune mon générateur avec val_data ou tout simplement utilise ses images 
@@ -57,7 +58,7 @@ class DatasetGenerator:
                     
                     images = self.generator.generate(batch)
 
-                    
+                    """""
                     image_input = processor(images=images, return_tensors="pt")  # Ajout
 
                     with torch.no_grad():
@@ -80,15 +81,15 @@ class DatasetGenerator:
                     self.save_images(images, label, image_id_0)            
                     image_id_0 += len(images)                               
                     pbar.update(1)
-                    """""
+        
 
                     
                 pbar.close()
 
-        
+        """""
         del model
         torch.cuda.empty_cache()
-        
+        """""
 
     def fine_tune(self,val_data,maping):
 
