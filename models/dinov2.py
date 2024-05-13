aa568b4a55dab0b996b629a5a08750b0d8085429
franck bambou <bamboufrancklio@gmail.com>
2024-05-13 08:38:28 +0200

    ze

--- models/dinov2.py
+++ models/dinov2.py
@@ -9,7 +9,7 @@ class DinoV2Finetune(nn.Module):
         self.backbone.head = nn.Identity()
         if frozen:
             for param in self.backbone.parameters():
-                param.requires_grad = False
+                param.requires_grad = True
             if unfreeze_last_layer:
                 for param in self.backbone.norm.parameters():
                     param.requires_grad = True
@@ -17,9 +17,9 @@ class DinoV2Finetune(nn.Module):
                     param.requires_grad = True
 
         self.features_dim = self.backbone.num_features
-        self.dropout = nn.Dropout(0.5)
+        self.dropout = nn.Dropout(0.7)
         self.batch_norm = nn.BatchNorm1d(self.features_dim)
-        self.activation = nn.ReLU()
+        #self.activation = nn.ReLU()
         self.classifier = nn.Linear(self.features_dim, num_classes)
        
 
@@ -27,7 +27,7 @@ class DinoV2Finetune(nn.Module):
         x = self.backbone(x)
         x = self.dropout(x)
         x = self.batch_norm(x)
-        x = self.activation(x)
+        #x = self.activation(x)
         x = self.classifier(x)
     
         return x
