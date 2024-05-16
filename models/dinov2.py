199fd66816f4d7ae532eba9fbcee077b7ea8cf40
franck bambou <bamboufrancklio@gmail.com>
2024-05-16 13:25:04 +0200

    e

--- models/dinov2.py
+++ models/dinov2.py
@@ -5,8 +5,8 @@ import torch.nn as nn
 class DinoV2Finetune(nn.Module):
     def __init__(self, num_classes, frozen=False, unfreeze_last_layer=True):
         super().__init__()
-        #self.backbone = torch.hub.load("facebookresearch/dinov2", "dinov2_vitb14_reg")
-        self.backbone= torch.hub.load('google/vit-base-patch16-224-in21k', 'vit_large_patch16_224', pretrained=True)
+        self.backbone = torch.hub.load("facebookresearch/dinov2", "dinov2_vitb14_reg")
+        #self.backbone= torch.hub.load('google/vit-base-patch16-224-in21k', 'vit_large_patch16_224', pretrained=True)
         self.backbone.head = nn.Identity()
         if frozen:
             for param in self.backbone.parameters():
@@ -21,8 +21,9 @@ class DinoV2Finetune(nn.Module):
 
         self.features_dim = self.backbone.num_features
         #self.dropout = nn.Dropout(0.7)
-        #self.batch_norm = nn.BatchNorm1d(self.features_dim)
-        #self.activation = nn.ReLU()
+        self.batch_norm = nn.BatchNorm1d(self.features_dim)
+        self.activation = nn.ReLU()
+        #self.classifier= nn.Linear(self.features_dim, num_classes)
         self.classifier = nn.Linear(self.features_dim, 768)
         self.activation = nn.ReLU()
         self.classifier1 = nn.Linear(768, num_classes)
@@ -31,7 +32,7 @@ class DinoV2Finetune(nn.Module):
     def forward(self, x):
         x = self.backbone(x)
         #x = self.dropout(x)
-        #x = self.batch_norm(x)
+        x = self.batch_norm(x)
         x = self.classifier(x)
         x = self.activation(x)
         x = self.classifier1(x)
