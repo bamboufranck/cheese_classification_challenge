a2cadfed486fa1b72de878b2344107950d48bd47
franck bambou <bamboufrancklio@gmail.com>
2024-05-21 20:56:28 +0200

    ze

--- data/dataset_generators/clip_prompts.py
+++ data/dataset_generators/clip_prompts.py
@@ -79,7 +79,7 @@ class ClipPromptsDatasetGenerator(DatasetGenerator):
         #llava
 
         prompt = "<|user|>\n<image>\nDescribe the image in sixty words, focusing primarily on the cheese and its surroundings, its location.<|end|>\n<|assistant|>\n"
-        model = LlavaForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.float16, low_cpu_mem_usage=True).to(device)
+        model = LlavaForConditionalGeneration.from_pretrained(model_id, torch_dtype=torch.float16, low_cpu_mem_usage=True)
         processor = AutoProcessor.from_pretrained(model_id)
 
 
