ec76b5ed428394c6e8ae88e7c9d06e5a2bf88afc
franck bambou <bamboufrancklio@gmail.com>
2024-05-23 21:37:56 +0200

    ze

--- generators/finetune_sdxl.py
+++ generators/finetune_sdxl.py
@@ -137,7 +137,7 @@ class FineTune_Sdxl:
             
         print("rafinage")
         
-        refined_output = self.refiner_pipe(prompts, image=images, num_inference_steps=80, guidance_scale=7.5)
+        refined_output = self.refiner_pipe(prompts, image=images, num_inference_steps=50, guidance_scale=0)
         refined_image = refined_output.images
        
 
